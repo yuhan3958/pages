7b5e0b80ad07e8d7f207548b9b31c9b5f29c471f
yuhan3958 <yuhan8954@gmail.com>
2025-03-28 14:32:37 +0900

    aaaa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-function sayhi(){
-  alert('Hi!')
+function sayhi(i){
+  if (i==1) {
+    alert('Hi!')
+  }
+  else if (i==2) {
+    alert('아니면 버그거나')
+  }
+  else {
+    alert("뭐야 님 개발자도구 뜯어봤죠")
+    sayhi(2)
+  }
 }
